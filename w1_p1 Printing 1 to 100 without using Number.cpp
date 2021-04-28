da58c347b3e7792634c6e1352568b646447895c4
Himanshu Shekhar <shekhar.leo.24@gmail.com>
2021-04-29 03:17:06 +0530

    Week 1 - Problem 1
    Printing 1 to 100 without using any number in C

---
+++ w1_p1 Printing 1 to 100 without using Number.cpp
@@ -0,0 +1,13 @@
+//Printing 1- 100 without using number.
+#include <iostream>;
+
+using namespace std;
+
+int main()
+{
+    for (int i = 'a' / 'a'; i <= 'd'; i++)
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
